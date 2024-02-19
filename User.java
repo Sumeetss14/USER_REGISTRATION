688ca078a6be6d996f122db11b7aa3cb56e58aa4
Sumeet Singh <87927732+Sumeetss14@users.noreply.github.com>
2024-02-19 21:37:05 +0530

    Delete User.java

--- User.java
+++
@@ -1,7 +0,0 @@
-package com.example.website.domain;
-
-public interface User {
-	public boolean createUser(String name,String gender,String location,String college);
-	public Integer saveUser();
-
-}
